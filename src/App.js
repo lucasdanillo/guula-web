1d2fc1eb2cecc9cfdd44ec34fad65925024cafa2
Gisele <giselesousar@gmail.com>
2020-07-03 20:51:41 -0300

    update changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
-import './styles.scss';
 import NavbarComponent from './components/Navbar';
-import {Container, Button} from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 function App() {
+
+  const [navbarColors, setNavbarColors] = useState({
+    backgroundColor: 'transparent',
+    color: "#616161"
+  });
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      window.scrollY > 150 ?
+        setNavbarColors({
+          backgroundColor: "orange",
+          color: "white"
+        })
+        :
+        setNavbarColors({
+          backgroundColor: "white",
+          color: "#616161"
+        })
+    })
+  }, [])
+
   return (
     <>
-    <NavbarComponent/>
+      <NavbarComponent color={navbarColors} />
       <div className="header" id="home">
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
-        <span id="title">Uhuuuuuul</span>
         <Container style={{
-            top: "80%",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            position: "absolute",
-            
-          }} >
-          <Button style={{
+          top: "70%",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          position: "absolute",
+
+        }} >
+          <Button size="lg" style={{
             color: "white",
             backgroundColor: "orange",
             borderColor: "orange",
-          }} 
-          href="#tutorial"
+          }}
+            href="#tutorial"
           >Como baixar o aplicativo</Button>
         </Container>
       </div>
-      <div className="tutorial" id="tutorial">
 
-      </div>
       <div className="about" id="about">
 
+      </div>
+      <div className="tutorial" id="tutorial">
+      
       </div>
       <div className="author" id="author">
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
-export default function NavbarComponent() {
+export default function NavbarComponent(props) {
     return (
-        <Navbar fixed="top" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar
+            fixed="top" 
+            expand="lg"
+            style={{
+                backgroundColor: props.color.backgroundColor,
+            }}
+        >
+            <Navbar.Brand href="#home" style={{
+                        color: props.color.color,
+                        fontSize: 30
+            }}>Guula</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav style={{ width: "100%" }} className="mr-auto justify-content-end">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#tutorial">Tutorial</Nav.Link>
-                    <Nav.Link href="#about">Sobre</Nav.Link>
-                    <Nav.Link href="#author">Autores</Nav.Link>
+                    <Nav.Link style={{
+                        color: props.color.color,
+                        fontSize: 25
+                    }} href="#home">Home</Nav.Link>
+                     <Nav.Link style={{
+                        color: props.color.color,
+                        fontSize: 25
+                    }} href="#about">Sobre</Nav.Link>
+                    <Nav.Link style={{
+                        color: props.color.color,
+                        fontSize: 25
+                    }} href="#tutorial">Tutorial</Nav.Link>
+                    <Nav.Link style={{
+                        color: props.color.color,
+                        fontSize: 25
+                    }} href="#author">Autores</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
