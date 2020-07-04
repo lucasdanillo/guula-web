0725f70a56235766a3f6b39861d92432734081fd
Gisele <giselesousar@gmail.com>
2020-07-03 22:32:50 -0300

    update changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
 import NavbarComponent from './components/Navbar';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Row, Col, Image } from 'react-bootstrap';
+import img1 from './assets/arthur.jpeg';
+import img2 from './assets/danillo.jpeg';
+import img3 from './assets/gisele.jpg';
+import img4 from './assets/samuel.jpeg';
+import { FiInstagram, FiGithub, FiMail } from 'react-icons/fi'
+
+
 function App() {
 
   const [navbarColors, setNavbarColors] = useState({
@@ -53,7 +60,131 @@ function App() {
       
       </div>
       <div className="author" id="author">
-
+          <Container  style={{
+            height: "100vh",
+          }}>
+            <Row>
+              <Container style={{
+                marginTop: 100,
+                textAlign: "center",
+              }}><span style={{
+                fontSize: 50,
+              }}>Autores</span>
+              </Container>
+              <Col style={{
+                marginTop: 50,
+              }}>
+                <Row>
+                  <Container style={{
+                  textAlign: "center"
+                }}>
+                <Image style={{width: "80%"}} src={img1} roundedCircle/>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                  textAlign: "center",
+                  marginTop: 20,
+                }}>
+                <span>Arthur Basílio</span>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                    textAlign: "center",
+                  }}>
+                    <FiGithub size={16} color="orange"/>
+                    <FiInstagram size={16} color="orange"/>
+                    <FiMail size={16} color="orange"/>
+                  </Container>
+                </Row>
+              </Col>
+              <Col style={{
+                marginTop: 50,
+              }}>
+                <Row>
+                  <Container style={{
+                  textAlign: "center"
+                }}>
+                <Image style={{width: "80%"}} src={img2} roundedCircle/>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                  textAlign: "center",
+                  marginTop: 20,
+                }}>
+                <span>Danillo Barros</span>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                    textAlign: "center",
+                  }}>
+                    <FiGithub size={16} color="orange"/>
+                    <FiInstagram size={16} color="orange"/>
+                    <FiMail size={16} color="orange"/>
+                  </Container>
+                </Row>
+              </Col>
+              <Col style={{
+                marginTop: 50,
+              }}>
+                <Row>
+                  <Container style={{
+                  textAlign: "center"
+                }}>
+                <Image style={{width: "80%"}} src={img3} roundedCircle/>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                  textAlign: "center",
+                  marginTop: 20,
+                }}>
+                <span>Gisele Ribeiro</span>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                    textAlign: "center",
+                  }}>
+                    <FiGithub size={16} color="orange"/>
+                    <FiInstagram size={16} color="orange"/>
+                    <FiMail size={16} color="orange"/>
+                  </Container>
+                </Row>
+              </Col>
+              <Col style={{
+                marginTop: 50,
+              }}>
+                <Row>
+                  <Container style={{
+                  textAlign: "center"
+                }}>
+                <Image style={{width: "80%"}} src={img4} roundedCircle/>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                  textAlign: "center",
+                  marginTop: 20,
+                }}>
+                <span>Samuel Santos</span>
+                </Container>
+                </Row>
+                <Row>
+                  <Container style={{
+                    textAlign: "center",
+                  }}>
+                    <FiGithub size={16} color="orange"/>
+                    <FiInstagram size={16} color="orange"/>
+                    <FiMail size={16} color="orange"/>
+                  </Container>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
       </div>
       <Container
         className="footer"
