7642c2c03a84c2353bdd38b60a0354ac377bc99f
Lucas Danillo <lucasdanillo1998@gmail.com>
2020-07-21 21:01:34 -0300

    Updates

---
+++ precache-manifest.ee992191445c19826575e9004d8be0b7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05875fcd509f20f9b08c0f7a6f3c556",
+    "url": "/guula-web/index.html"
+  },
+  {
+    "revision": "b1f1fabd7761f834cd09",
+    "url": "/guula-web/static/css/main.b80cf5d9.chunk.css"
+  },
+  {
+    "revision": "0550beb4b606c2cc8882",
+    "url": "/guula-web/static/js/2.58915f7a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/guula-web/static/js/2.58915f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f1fabd7761f834cd09",
+    "url": "/guula-web/static/js/main.62e51dc4.chunk.js"
+  },
+  {
+    "revision": "e1e0b79b50865ebeac13",
+    "url": "/guula-web/static/js/runtime-main.0a4d466a.js"
+  },
+  {
+    "revision": "dde6ff87f7cd952452d63d86aceb0c56",
+    "url": "/guula-web/static/media/1740.dde6ff87.jpg"
+  },
+  {
+    "revision": "e596c3f959eaf817a25352f123549fbc",
+    "url": "/guula-web/static/media/arthur.e596c3f9.jpeg"
+  },
+  {
+    "revision": "17298adf1348cc9a70a86d73dd27953a",
+    "url": "/guula-web/static/media/danillo.17298adf.jpeg"
+  },
+  {
+    "revision": "ae485e29d1fcbb5d452b68c4824eef31",
+    "url": "/guula-web/static/media/downloadAppStore.ae485e29.png"
+  },
+  {
+    "revision": "14be07842c83c85e501a4134d73a4fde",
+    "url": "/guula-web/static/media/downloadGooglePlay.14be0784.png"
+  },
+  {
+    "revision": "1b0b65d2a41d546014641cf96946e496",
+    "url": "/guula-web/static/media/gisele.1b0b65d2.jpg"
+  },
+  {
+    "revision": "49ba13327b2ee93f262835efc4306aad",
+    "url": "/guula-web/static/media/icon.49ba1332.png"
+  },
+  {
+    "revision": "24fe997c205fd01c019de907f6445e5e",
+    "url": "/guula-web/static/media/samuel.24fe997c.jpeg"
+  },
+  {
+    "revision": "ae1e0c393506028bc874e8488bfe1fb4",
+    "url": "/guula-web/static/media/teste.ae1e0c39.png"
+  }
+]);
